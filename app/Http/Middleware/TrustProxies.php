d0b4e923dec43555bf0281bfe2efb54dd971323a
baijian <baijian@xingwenzn.com>
2025-01-03 10:46:36 +0800

    heroku/heroku-buildpack-php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
-use Symfony\Component\HttpFoundation\Request as RequestAlias;
 
 class TrustProxies extends Middleware
 {
@@ -20,5 +19,11 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $headers = RequestAlias::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
 }
